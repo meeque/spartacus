6e19b8c3edc8f86fae17ea265fbf47e4b594241a
Michael Riedel <michael.riedel01@sap.com>
2022-08-01 12:57:07 +0200

    CXSECUR5-9376: Use Location API for URL manipulation in cxSupplementHashAnchors. Also improve security documentation.

--- projects/storefrontlib/shared/pipes/suplement-hash-anchors/supplement-hash-anchors.pipe.ts
+++ projects/storefrontlib/shared/pipes/suplement-hash-anchors/supplement-hash-anchors.pipe.ts
@@ -11,20 +11,24 @@ import { WindowRef } from '@spartacus/core';
  * when clicking the original link.
  *
  * It's useful for example for cms-provided content passed to the [innerHTML] directive.
+ *
+ * This pipe does not guarantee that the resulting html source code snippet will be safe for use.
+ * That is, if the inputs to the pipe contain undesired JavaScript code, the outputs will contain undesired JavaScript code, too.
+ * Do NOT bypass Angular's html sanitization when passing the results of this pipe to the browser for rendering!
  */
 @Pipe({ name: 'cxSupplementHashAnchors' })
 export class SupplementHashAnchorsPipe implements PipeTransform {
   constructor(protected renderer: Renderer2, protected winRef: WindowRef) {}
 
-  protected getPath(anchorId: string): string {
-    const currentUrlWithoutFragment = this.winRef.location.href?.replace(
-      /#.*$/,
-      ''
-    );
-    return `${currentUrlWithoutFragment}${anchorId}`;
+  protected getAbsoluteUrl(relativeUrl: string): string {
+    const absoluteUrl = new URL(relativeUrl, this.winRef.location.href);
+    return absoluteUrl.href;
   }
 
   public transform(html: string = ''): string {
+    // the following code is secure, because:
+    // 1. <template> element contents are inert and will not execute JS event handlers upon DOM manipulation
+    // 2. the returned HTML source code snippet will be subject to standard Angular html sanitization mechanisms
     const template = this.renderer.createElement('template');
     template.innerHTML = html.trim();
     const linkNodes: NodeListOf<HTMLAnchorElement> =
@@ -33,7 +37,7 @@ export class SupplementHashAnchorsPipe implements PipeTransform {
     Array.from(linkNodes).forEach((link: HTMLAnchorElement) => {
       const href = link.getAttribute('href');
       if (href?.indexOf('#') === 0) {
-        this.renderer.setProperty(link, 'href', this.getPath(href));
+        this.renderer.setAttribute(link, 'href', this.getAbsoluteUrl(href));
       }
     });
     return template.innerHTML;
